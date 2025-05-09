b1bacf2c8e2ba782d9fcd27afb0665ec22bd1b18
German Merlo <germanmerlo17@gmail.com>
2025-05-09 15:51:34 -0300

    mas ejercicios

--- Practica-JS/app.js
+++ Practica-JS/app.js
@@ -1,4 +1,4 @@
-//Primer ejercicio:
+//Primeros ejercicios:
 //      ***Crear una función que reciba dos números y devuelva su suma.***
 function suma(a, b){
     return (a + b);
@@ -39,4 +39,65 @@ function invertirCadena(texto){
     return texto.split("").reverse().join("")
 }
 
-console.log(invertirCadena("probando si si"))
+//console.log(invertirCadena("probando si si"))
+
+
+//      ***Tabla de multiplicar (del 1 al 10), concatenación y template literals***
+function tablaMult(num){
+    for (let i = 1; i <=10; i++){
+        console.log(num + " * " + i + " = " + i*num)
+    }
+}
+//tablaMult(9)
+
+//Solucion con template literals:
+function multiplicar(num){
+    for (let i = 1; i <=10; i++){
+        console.log(`${num} * ${i} = ${num*i}`)
+    }
+}
+//multiplicar(8)
+
+
+//          ***Escribí una función que reciba un array y devuelva la suma de todos los elementos numéricos***
+
+function sumaArreglo(arreglo){
+    let resultado = 0;
+    for (let i = 0; i < arreglo.length; i++){
+        let posicion = arreglo[i];
+
+        if(typeof posicion === "number" && Number.isFinite(posicion)){
+            resultado += posicion
+        }
+        else{
+            console.warn(`Elemento no tenido en cuenta '${posicion}' porque es de tipo: ${typeof posicion}`)
+        }
+    }
+    return `El resultado de la suma es ${resultado}`
+}
+
+/* pruebaSumaArreglo = sumaArreglo([1, 18, 22, "hola", true, 9 , "ocho", "dos", 5, "3"])
+console.log(pruebaSumaArreglo)
+ */
+
+//          ***Buscar el mayor de un array***
+
+function findMax(arreglo){
+    let maximo = 0
+
+    for (let i = 0; i < arreglo.length; i++){
+        let comparar = arreglo[i]
+
+        if (typeof comparar === "number"){
+            if (comparar > maximo){
+                maximo = comparar
+            }
+        }
+        else{
+            console.warn(`Este elemento: '${comparar}', se ignoró porque es de tipo ${typeof comparar}`)
+        }
+    }
+    return `El numero mayor en el arreglo es el ${maximo}`
+}
+arregloPrueba = [1, 18, 22, "hola", true, 9 , "ocho", "dos", 5, "3"]
+console.log(findMax(arregloPrueba))
