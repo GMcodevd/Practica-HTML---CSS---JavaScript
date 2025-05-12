23a80b79481f6f590b976ca39f23b41b8ea261e5
German Merlo <germanmerlo17@gmail.com>
2025-05-11 23:48:11 -0300

    Algunas funciones mas, clase subtitulo y clase parrafo

--- Manejo de eventos/app.js
+++ Manejo de eventos/app.js
@@ -3,8 +3,9 @@ const saludo = document.getElementById('unSaludo');
 
 // Agregar evento
 saludo.addEventListener('click', function(){
-    alert('Hola desde JavaScript!!')
+    alert('Tocaste el título de la página!! :p')
 });
+
 
 //seleccionamos elementos
 const formulario = document.getElementById("miFormulario");
@@ -31,5 +32,36 @@ formulario.addEventListener("submit", function(evento){
         nombre: nombreInput.value,
         email: emailInput.value
     });
+    confirm("Se enviarán los datos")
+    alert("Tus datos se enviaron con éxito ;)")
 });
 
+//Agregamos mas elementos: clases and ids
+const classSubtitulos = document.querySelectorAll(".subtitulo");
+const classParrafos = document.querySelectorAll(".parrafo");
+const cambiarColor = document.querySelector("#cambiarColor");
+const primerSub = document.getElementById("primerSubtitulo");
+const segundoSub = document.getElementById("segundoSubtitulo");
+const tercerSub = document.getElementById("tercerSubtitulo");
+const cuartoSub = document.getElementById("cuartoSubtitulo");
+const botonRojo = document.getElementById("botonRojo");
+const botonSimple = document.querySelector(".boton");
+
+//Agregamos eventos:
+classSubtitulos.forEach(function(subtitulo){        // Para recorrer cada elemento dentro de la clase
+    subtitulo.addEventListener("click", function(){
+        alert("Tocaste un subtítulo!");
+    });
+});
+
+classParrafos.forEach(function(parrafo){
+    parrafo.addEventListener("click", function(){
+        const colorActual = parrafo.style.color;
+
+        if (colorActual === "" || colorActual === "black") {
+            parrafo.style.color = "blue";
+        } else if (colorActual === "blue"){
+            parrafo.style.color = "black";
+        }
+    })
+})
